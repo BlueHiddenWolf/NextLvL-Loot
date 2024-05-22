d3c748d8967bb8e5fb94f94e81923d793fa82f4a
freddiedlx <161600523+freddiedlx@users.noreply.github.com>
2024-05-22 13:59:01 +0000

    WorkingVersion2.0

--- game/src/main/java/ch/fhnw/game/PizzaApplication.java
+++ game/src/main/java/ch/fhnw/game/PizzaApplication.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.fhnw.game.business.service.CatalogService;
-import ch.fhnw.game.business.service.MenuService;
 import ch.fhnw.game.data.domain.Game;
-import ch.fhnw.game.data.domain.Pizza;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.annotation.PostConstruct;
 
